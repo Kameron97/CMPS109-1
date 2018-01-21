3961c1f2f43a0a1e0f10188dab04802566756a11
Aravind Patnam <akpatnam@shyams-iMac.T-mobile.com>
2018-01-20 20:10:26 -0800

    cheese

--- asg1/bigint.cpp
+++ asg1/bigint.cpp
@@ -322,25 +322,16 @@ bigvalue_t partial_rem(const bigvalue_t& x, size_t k) {
 
 digit_t trialdigit(const bigvalue_t& r, const bigvalue_t& d, size_t k, size_t m) {
     int d2, r3;
-    size_t km = k + m;
+    size_t km;
+    km = k + m;
     if (r.size() > km)
-        r3 = (r.at(km)*10 + r.at(km - 1)) * 10 + r.at(km - 2);
+        r3 = (r.at(km)*10 + r.at(km - 1))*10 + r.at(km - 2);
     else if (r.size() > km - 1)
-        
+        r3 = r.at(km - 1)*10 + r.at(km - 2);
     else if (r.size() > km - 2)
         r3 = r.at(km - 2);
     else
-        
-
-    if (r.size() > km - 1) {
-        r3 = r.at(km - 1)*10 + r.at(km - 2);
-    } else if (r.size() > km - 2) {
-        r3 = r.at(km - 2);
-    } else if (r.size() > km) {
-        r3 = (r.at(km)*10 + r.at(km - 1)) * 10 + r.at(km - 2);
-    } else {
         r3 = 0;
-    }
 
     if (d.size() > m - 1)
         d2 = d.at(m - 1)*10 + d.at(m - 2);
