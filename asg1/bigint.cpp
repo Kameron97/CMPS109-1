3039653ddb97be29e393b12f880c7ffa6a06702e
Aravind Patnam <akpatnam@shyams-iMac.T-mobile.com>
2018-01-21 16:10:58 -0800

    cheese

--- asg1/bigint.cpp
+++ asg1/bigint.cpp
@@ -288,12 +288,12 @@ bigvalue_t partial_rem(const bigvalue_t& x, size_t k) {
 digit_t trialdigit(const bigvalue_t& r, const bigvalue_t& d, size_t k, size_t m) {
     int d2, r3;
     size_t km = k + m;
-
+/*
     if (r.size() > km) {
         r3 = (r.at(km)*10 + r.at(km - 1))*10 + r.at(km - 2);
-    } if (r.size() > km - 1) {
+    } else if (r.size() > km - 1) {
         r3 = r.at(km - 1)*10 + r.at(km - 2);
-    } if (r.size() > km - 2) {
+    } else if (r.size() > km - 2) {
         r3 = r.at(km - 2);
     } else {
         r3 = 0;
@@ -306,8 +306,29 @@ digit_t trialdigit(const bigvalue_t& r, const bigvalue_t& d, size_t k, size_t m)
     } else {
         d2 = 0;
     }
-
+*/
+    discoverDigit(r, d, k, m);
     return min(r3 / d2, 9);
+}
+
+digit_t discoverDigit(const bigvalue_t& r, const bigvalue_t& d, size_t k, size_t m) {
+    if (r.size() > km) {
+        r3 = (r.at(km)*10 + r.at(km - 1))*10 + r.at(km - 2);
+    } else if (r.size() > km - 1) {
+        r3 = r.at(km - 1)*10 + r.at(km - 2);
+    } else if (r.size() > km - 2) {
+        r3 = r.at(km - 2);
+    } else {
+        r3 = 0;
+    }
+
+    if (d.size() > m - 1) {
+        d2 = d.at(m - 1)*10 + d.at(m - 2);
+    } else if (d.size() > m - 2) {
+        d2 = d.at(m - 2);
+    } else {
+        d2 = 0;
+    }
 }
 
 
